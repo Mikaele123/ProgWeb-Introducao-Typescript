90e7d9a80b40e5d131786f9810cbdc6d87fba0a9
Mikaele Nascimento da Silva <mikaele.n@estudante.ifro.edu.br>
2025-06-04 17:04:52 -0400

    Introdução ao Typescript - parte2

---
+++ estruturas de controle.ts
@@ -0,0 +1,16 @@
+// === ESTRUTURAS DE CONTROLE ===
+
+// IF - Se
+// ELSE IF - SENÃO SE
+
+let idade: number = 17;
+
+if (idade >= 18) {
+    //ação
+    console.log('Maior de idade');
+    
+}else if(idade >= 16){
+    console.log('Pode votar');
+}else{
+    console.log('Menor de idade');
+}
